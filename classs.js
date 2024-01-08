2111e61c7c4e69fe351feb87457aaf0983d2d764
khalidadamu <khalidbox720@icloud.com>
2024-01-08 06:55:09 +0100

    getter and setter method

--- classs.js
+++ classs.js
@@ -1,6 +1,6 @@
 // TODO: declaring classes
 
-// class Person ggg {
+// class Person  {
 //   constructor(name, age) {
 //     console.log(this);
 //     this.name = name;
@@ -8,11 +8,25 @@
 //   }
 // }
 
+
+
 // const person = new Person();
 // console.log(person);
 
+// TODO class intanciations
+class khalid { 
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
 
-// Class methods
+  }
+}
+
+const person5 = new khalid('Khalid', 45);
+console.log(person5);
+
+
+// TODO: Class methods
 // The constructor inside a class is a builtin function which allow us to create a blueprint for the object. In a class we can create class methods. Methods are JavaScript functions inside the class. Let us create some class methods.
 
 class Person {
@@ -34,3 +48,95 @@ const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
 
 console.log(person1.getFullName())
 console.log(person2.getFullName())
+
+
+// TODO: getter
+// The get method allow us to access value from the object. We write a get method using keyword get followed by a function. Instead of accessing properties directly from the object we use getter to get the value. See the example bellow
+
+class Person {
+  constructor(firstName, lastName, age, country, city) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+    this.score = 0
+    this.skills = []
+  }
+  getFullName() {
+    const fullName = this.firstName + ' ' + this.lastName
+    return fullName
+  }
+  get getScore() {
+    return this.score
+  }
+  get getSkills() {
+    return this.skills
+  }
+}
+
+const person70 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+const person50 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+
+
+// //TODO: Setter
+// The setter method allow us to modify the value of certain properties. We write a setter method using keyword set followed by a function. See the example bellow.
+
+class Person {
+  constructor(firstName, lastName, age, country, city) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+    this.score = 0
+    this.skills = []
+  }
+  getFullName() {
+    const fullName = this.firstName + ' ' + this.lastName
+    return fullName
+  }
+  get getScore() {
+    return this.score
+  }
+  get getSkills() {
+    return this.skills
+  }
+  set setScore(score) {
+    this.score += score
+  }
+  set setSkill(skill) {
+    this.skills.push(skill)
+  }
+}
+
+// const person1 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+// const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+
+// person1.setScore = 1
+// person1.setSkill = 'HTML'
+// person1.setSkill = 'CSS'
+// person1.setSkill = 'JavaScript'
+
+// person2.setScore = 1
+// person2.setSkill = 'Planning'
+// person2.setSkill = 'Managing'
+// person2.setSkill = 'Organizing'
+
+// TODO: Inheritance
+// Using inheritance we can access all the properties and the methods of the parent class. This reduces repetition of code. If you remember, we have a Person parent class and we will create children from it. Our children class could be student, teach etc.
+
+
+// Let us create a Student child class from Person parent class.
+
+class Student extends Person {
+  saySomething() {
+    console.log('I am a child of the person class')
+  }
+}
+
+const s1 = new Student('Asabeneh', 'Yetayeh', 'Finland', 250, 'Helsinki')
+console.log(s1)
+console.log(s1.saySomething())
+console.log(s1.getFullName())
+console.log(s1.getPersonInfo())
